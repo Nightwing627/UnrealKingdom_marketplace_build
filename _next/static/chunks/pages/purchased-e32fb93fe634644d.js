03a99d9c46a36a04d40107d7a53f3e5c66cce738
Nightwing627 <webdev0627>
2022-11-01 04:54:39 -0500

    marketplace first build

---
+++ _next/static/chunks/pages/purchased-e32fb93fe634644d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[106],{9521:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/purchased",function(){return u(5735)}])}},function(n){n.O(0,[543,245,735,774,888,179],(function(){return _=9521,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
